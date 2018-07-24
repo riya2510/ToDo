692e5c0fd701494f7ae46d10eb431b28c4618427
vishwa patel <vishwa.patel@rapidops.com>
2018-07-24 15:51:10 +0530

    add directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
-import { ViewTodoComponent } from './view-todo/view-todo.component';
 import { DetailTodoComponent } from './detail-todo/detail-todo.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     AddTodoComponent,
-    ViewTodoComponent,
-    DetailTodoComponent
+    DetailTodoComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
